0f9f192a6755492346416063319bb211a8d7423c
iioUHoD <iiouhod@gmail.com>
2022-10-25 16:48:13 +0700

    Initial commit

---
+++ src/main/java/com/example/projectservice2/ProjectService2Application.java
@@ -0,0 +1,13 @@
+package com.example.projectservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectService2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectservice2/ProjectService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
